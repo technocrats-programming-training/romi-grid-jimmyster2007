34b8d02f9b06a4d32c24e63eed2fed11b729a137
jimmyster2007 <93486196+jimmyster2007@users.noreply.github.com>
2022-06-30 23:21:20 +0000

    passed

--- src/main/java/frc/robot/Position.java
+++ src/main/java/frc/robot/Position.java
@@ -4,29 +4,36 @@ import static lib.Romi.driveDistance;
 import static lib.Romi.turnDegrees;
 
 public class Position {
-
+    
+    int verticalPosition;
+    int horizontalPosition;
+    int currentAngle;
 
     public void driveUp() throws InterruptedException {
-
+      // if currentAngle = 0 then driveDistance(10) then verticalPosition += 10 else turnDegrees(90) and currentAngle + 90  if currentAngle = 0 then driveDistance(10) then verticalPosition += 10 else turnDegrees(90) and currentAngle + 90 if currentAngle = 0 then driveDistance(10) then verticalPosition += 10 else turnDegrees(90) and currentAngle + 90;
+        verticalPosition += 10;
     }
 
     public void driveDown() throws InterruptedException {
-
+      
+      verticalPosition -= 10;
     }
 
     public void driveRight() throws InterruptedException {
-
+      
+      horizontalPosition += 10;
     }
 
     public void driveLeft() throws InterruptedException {
-
+      turnDegrees(-90);
+      horizontalPosition -= 10;
     }
 
     public void returnToHome() {
-
+      
     }
 
     public String toString() {
-        return "";
+        return "(" + horizontalPosition + ", " + verticalPosition + ")";
     }
 }
